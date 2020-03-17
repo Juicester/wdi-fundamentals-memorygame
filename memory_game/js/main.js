62a5b7ee41bf9cc32c76bd0746d489eca09aae50
Justin Pedowitz <justin@pedowitz.net>
2020-03-17 16:22:47 -0700

    Intro To Javascript Project. Created Game Logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,10 @@
+// Main Javascript File
+console.log("Up and running!");
+
+let cardOne = "queen";
+let cardTwo = "queen";
+let cardThree = "king";
+let cardFour = "king";
+
+console.log("User flipped " + cardTwo);
+console.log("User flipped " + cardFour);
