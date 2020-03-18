443f2a808c7fb8bef0a9b54b92c93f59e3ae7811
Justin Pedowitz <justin@pedowitz.net>
2020-03-18 12:38:59 -0700

    Functions Deliverable. Added game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,23 +1,27 @@
 // Main Javascript File
-console.log("Up and running!");
+//console.log("Up and running!");
 
 let cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
 
-let cardOne = cards[0];
-let cardTwo = cards[3];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardsInPlay[0]);
-console.log("User flipped " + cardsInPlay[1]);
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match.");
-	} 
-	else {
-		alert("Sorry, try again.");
+function checkForMatch() {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			console.log("You found a match.");
+		} 
+		else {
+			console.log("Sorry, try again.");
+		}
 	}
 }
+
+function flipCard(cardID) {
+	console.log("User flipped " + cards[cardID]);
+
+	cardsInPlay.push(cards[cardID]);
+
+	checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
